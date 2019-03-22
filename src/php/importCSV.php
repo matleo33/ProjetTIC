72c1271248945c8aed363c823c9ed83f114415d1
matleo33 <mathieu.leothaud@etu.u-bordeaux.fr>
2019-03-22 14:52:56 +0100

    oubli

--- src/php/importCSV.php
+++ src/php/importCSV.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_FILES['userfile'])) {
-    $content=file_get_contents($_FILES);
+    $content=file_get_contents($_FILES['userfile']);
 }
 echo $content;
 //header('Location: ../html/index.php');
